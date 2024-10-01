05d111407b8ff1ce0bf4d31d6097ab7bed49e0e9
aksprat <121097688+aksprat@users.noreply.github.com>
2024-10-01 11:02:12 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,34 +1,27 @@
-document.getElementById('btn').addEventListener('click', async () => {
-    const prompt = document.getElementById('input').value;
-    const loadingElement = document.getElementById('loading');
-    const imageElement = document.getElementById('image');
-    
-    // Show the loading spinner
-    loadingElement.style.display = 'block';
-    imageElement.style.display = 'none';
+const inputTxt = document.getElementById("input")
+const image = document.getElementById("image")
+const button = document.getElementById("btn")
 
-    // Fetch the generated image from your Flask back-end
-    try {
-        const response = await fetch("/generate", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ prompt })
-        });
+async function query(data) {
+    image.src = "/loading.gif"
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
+		{
+			headers: {
+				Authorization: "Bearer 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" ,
+				"Content-Type": "application/json",
+			},
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.blob();
+	return result;
+}
+button.addEventListener('click', async function () {
+    query({"inputs": inputTxt.value}).then((response) => {
+        const objectURL = URL.createObjectURL(response)
+        image.src = objectURL
+    });
 
-        if (response.ok) {
-            const blob = await response.blob();
-            const imageUrl = URL.createObjectURL(blob);
-            imageElement.src = imageUrl;
-            imageElement.style.display = 'block'; // Display the image
-        } else {
-            console.error("Error generating image", response.statusText);
-        }
-    } catch (error) {
-        console.error("Error: ", error);
-    } finally {
-        // Hide the loading spinner
-        loadingElement.style.display = 'none';
-    }
-});
+})
